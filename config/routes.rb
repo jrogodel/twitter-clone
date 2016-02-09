5f2e21816c56687aba3a412a2e049989df88cf06
jrg <jrg@victoria-godels-macbook.local>
2016-02-09 02:54:25 -0500

    Add Users Controller Test and configured the sign up pages routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'users/new'
 
   root              'static_pages#home'
+  get 'signup'	=>	'users#new'
   get 'help'    =>  'static_pages#help'
   get 'about'   =>  'static_pages#about'
   get 'contact' =>  'static_pages#contact'
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,9 +1,15 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
-  test "should get new" do
+
+  def setup
+    @base_title = "Ruby on Rails Twitter Clone"
+  end
+
+  test "should get signup" do
     get :new
     assert_response :success
+    assert_select "title", "Sign Up | #{@base_title}"
   end
 
 end
