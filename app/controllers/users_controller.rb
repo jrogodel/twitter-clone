c22cfe01a57d42ce26c251bc1ece1f3d3020b6bc
Jamie Godel <jamiegodel@yahoo.com>
2016-02-18 05:42:20 -0500

    Complete user sign in functionality and include TDD

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,8 @@ class UsersController < ApplicationController
 	def create
 		@user = User.new(user_params)
 		if @user.save
-			# redirect_to
+			redirect_to @user
+			flash[:success] = "Welcome to Tweeter!"
 		else
 			render 'new'
 		end
@@ -22,5 +23,6 @@ class UsersController < ApplicationController
 	  	def user_params
 	  		params.require(:user).permit(:name, :email, :password, :password_confirmation)
 	  	end
-		end
+		
 end
+	
